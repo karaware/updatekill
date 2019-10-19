645040ad83fac08d5a50a126a0380387eb92ca7f
root <root@localhost.localdomain>
2019-10-19 07:05:48 +0000

    modify updatekill.sh

--- updatekill.sh
+++ updatekill.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 
-export LANG=c;
+export LANG=C;
 
 LOGDIR="./log";
 GREP_STR="UPDATE";
 TIME_OUT="10";
 
 MYSQL="mysql --defaults-extra-file=./config/dbaccess.conf"
-SHOW="show full processlist;"
 TMP1=./tmp.txt;
 
 NOW=`date +"%Y%m%d_%p_%I%M%S"`
 TODAY=`date +"%Y%m%d";`
-KILL_TARGET_FILE="$LOGDIR/kill_$NOW.txt";
 LOG_FILE="$LOGDIR/kill_$TODAY.log";
 
-echo $SHOW | $MYSQL | grep "$GREP_STR" > $TMP1
+$MYSQL -e "show full processlist;" | grep "$GREP_STR" > $TMP1
 
 echo "--- $NOW ---" >> $LOG_FILE;
 while read line;
@@ -25,11 +23,9 @@ do
 
     if [ $time -gt $TIME_OUT ];
     then
-        echo "kill $pid;" >> $KILL_TARGET_FILE;
         echo "$line" >> $LOG_FILE;
-        $MYSQL < $KILL_TARGET_FILE;
-         echo "pid=$pid,time=$time killed";
-        rm $KILL_TARGET_FILE;
+        $MYSQL -e "kill $pid;"
+        echo "pid=$pid,time=$time killed";
     fi;
 done < $TMP1;
 rm $TMP1;
